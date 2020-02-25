897f502c440e6ceb5eb1d0ff24e420ddf5894e82
Enrique Pittaluga <ep7m@me.com>
2020-02-25 02:00:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   newarr = []
   while counter < array.length do
-    newarr = Math.power(2, array[counter])
+    newarr = array[counter]**2
     counter += 1
   end
   return newarr
